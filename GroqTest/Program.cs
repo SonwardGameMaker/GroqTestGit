6033377033f26d95a0b50f26055275e9fcd1e69b
SonwardGameMaker <sonward.gamemaker@gmail.com>
2025-06-19 09:12:32 +0300

    Update Program.cs

--- GroqTest/Program.cs
+++ GroqTest/Program.cs
@@ -9,19 +9,19 @@ class Program
         string apiKey = "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";
         string apiKey2 = "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";
 
-        GroqService groqService = new GroqService(apiKey, GroqModel.LLaMA3_70b);
+        GroqService groqService = new GroqService(apiKey2, GroqModel.LLaMA3_70b);
         GroqChatHistory groqMessages = new GroqChatHistory { BotInstructions.DefaultInstructions.BaseInstructions };
         UserCurrentData userCurrentData = new UserCurrentData("Roman");
 
-        //Console.WriteLine($"Groq reach: {await groqService.TestGroqReachabilityAsync() }");
-        Console.WriteLine($"Groq POST: {await groqService.TestGroqPostAsync(apiKey2)}");
+        ////Console.WriteLine($"Groq reach: {await groqService.TestGroqReachabilityAsync() }");
+        //Console.WriteLine($"Groq POST: {await groqService.TestGroqPostAsync(apiKey2)}");
 
-        //Console.WriteLine("Without history //----------------------------------------------------------------------");
-        //string message1 = "Hello";
-        //Console.WriteLine($"User prompt: {message1}");
-        //var response1 = await groqService.AskAsync(message1);
-        //Console.WriteLine("Groq response:");
-        //Console.WriteLine(response1);
+        Console.WriteLine("Without history //----------------------------------------------------------------------");
+        string message1 = "Hello";
+        Console.WriteLine($"User prompt: {message1}");
+        var response1 = await groqService.AskAsync(message1);
+        Console.WriteLine("Groq response:");
+        Console.WriteLine(response1);
 
         //Console.WriteLine("With history //----------------------------------------------------------------------");
         //string message2 = "Hello, did I Confirmed my passport?";
