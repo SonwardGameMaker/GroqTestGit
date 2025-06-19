920cf6bdbcfd386856d8f2e1731771beb9ac2262
SonwardGameMaker <sonward.gamemaker@gmail.com>
2025-06-19 09:08:02 +0300

    Update Program.cs

--- GroqTest/Program.cs
+++ GroqTest/Program.cs
@@ -7,13 +7,14 @@ class Program
     static async Task Main(string[] args)
     {
         string apiKey = "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";
+        string apiKey2 = "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";
 
         GroqService groqService = new GroqService(apiKey, GroqModel.LLaMA3_70b);
         GroqChatHistory groqMessages = new GroqChatHistory { BotInstructions.DefaultInstructions.BaseInstructions };
         UserCurrentData userCurrentData = new UserCurrentData("Roman");
 
         //Console.WriteLine($"Groq reach: {await groqService.TestGroqReachabilityAsync() }");
-        Console.WriteLine($"Groq POST: {await groqService.TestGroqPostAsync(apiKey)}");
+        Console.WriteLine($"Groq POST: {await groqService.TestGroqPostAsync(apiKey2)}");
 
         //Console.WriteLine("Without history //----------------------------------------------------------------------");
         //string message1 = "Hello";
